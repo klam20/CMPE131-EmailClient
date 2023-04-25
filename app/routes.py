797c1089efbec36ca4fd216709923b63c3a86e3f
Jeffrey Lam <jeffrey.lam@sjsu.edu>
2023-04-24 21:39:32 -0700

    Form validation working with flash messages

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
 from flask import render_template
+from flask import flash
+from flask import redirect
 from .forms import LoginForm
 from app import myapp_obj
+from flask_login import current_user
+from flask_login import login_user
+from flask_login import logout_user
+from flask_login import login_required
 
 @myapp_obj.route("/")
 
@@ -12,10 +18,13 @@ def home():
 def email():
     return render_template('email.html')
 
-@myapp_obj.route("/login")
-def login():
-    current_form = LoginForm()
-    return render_template('login.html', form=current_form)
+@myapp_obj.route("/login", methods=['GET','POST'])
+def login():    
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Here are the input {form.username.data} and {form.password.data}')
+        return redirect('/login')
+    return render_template('login.html', form=form)
 
 @myapp_obj.route("/register")
 def register():
