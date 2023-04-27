c0a8ad398d4a581d7b859bfebdf06acd031d047c
Anthony Nguyen <antnguyen233@gmail.com>
2023-04-27 03:28:55 -0700

    Changes to email page html for todo list + database for tasks(still broken)

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from app import db
+
+class task(db.Model):
+    task_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32))
+    date = db.Column(db.String(32))
+    done = db.Column(db.Boolean)
+
+    def set_name(self,taskDisc):
+        self.name = taskDisc
+
+    def set_date(self, dueDate):
+        self.date = dueDate
