60b6596c36f33446d2e8a4b00a26170719037a49
Nicole Nacua <nicole.nacua@sjsu.edu>
2023-04-28 16:01:55 -0700

    Got the db to workgit add .

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,9 @@
-from flask import render_template,flash, redirect
-from flask_sqlalchemy import SQLAlchemy
-from app import myapp_obj
-from app import db
-from app.models import *
+from flask import render_template, flash, redirect, url_for
+from app import myapp_obj, db
+from app.models import Register
 from .forms import RegistrationForm
 
 @myapp_obj.route("/")
-
 @myapp_obj.route("/home")
 def home():
     return render_template('home.html')
@@ -18,24 +15,18 @@ def email():
 @myapp_obj.route("/login")
 def login():
     return render_template('login.html')
-
+    
 @myapp_obj.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegistrationForm()    
-    db.create_all()
-    
-    new_user = Register(email= "fml@gmail.com")
-
-    new_user.set_password('123')
-    db.session.add(new_user)
-    db.session.commit()   
+    form = RegistrationForm()
 
     if form.validate_on_submit():
-        new_user = User(email=form.email.data)
+        new_user = Register(email=form.email.data)
         new_user.set_password(form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
         flash('You are now a registered user!')
         return redirect(url_for('login'))
+
     return render_template('register.html', title='Register', form=form)
-
-
 
