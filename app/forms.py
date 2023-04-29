68f01d20ff30604b2beef5debcddf02e00e1ce38
Bryant Bautista <bbautistabryant@gmail.com>
2023-04-28 18:45:27 -0700

    Updated forms.py file

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,12 @@
 from flask_wtf import FlaskForm
 from app import *
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # empty class, passes form
 class ChatForm(FlaskForm):
     pass
+
+class AddRecipientForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    submit = SubmitField("Add Recipient")
