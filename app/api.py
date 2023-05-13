091b72a065f9d44c3b8a7552c05494b536d7c25f
Jeffrey Lam <jeffrey.lam@sjsu.edu>
2023-05-13 14:14:23 -0700

    Migrating style.css into respective css files

--- app/api.py
+++ app/api.py
@@ -1,10 +1,12 @@
 import requests
 
+#FROM API-NINJA
+#Generates a 16 character password
 def generatePassword():
     length = '16'
     api_url = 'https://api.api-ninjas.com/v1/passwordgenerator?length={}'.format(length)
     response = requests.get(api_url, headers={'X-Api-Key': 'bdPWsaINX/n3R+hrCS3wtw==Y6dj8yHSwpljaAXh'})
     if response.status_code == requests.codes.ok:
-        return response.text[21:-2]
+        return response.text[21:-2] #Need to substring in order to get actual PW and not other information
     else:
         print("Error:", response.status_code, response.text)
